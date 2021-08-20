291e488e48c57027d5641848a0829bc8b9e01b21
Jordan <jsha773@hotmail.com>
2021-08-20 15:29:24 +1000

    New create.php for creating/inserting records

---
+++ classes/form/create_entry.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_cpdlogbook\form;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/formslib.php');
+
+class create_entry extends \moodleform {
+    public function definition() {
+        $mform = $this->_form;
+
+        $mform->addElement('text', 'name', get_string('entryname', 'mod_cpdlogbook'));
+        $mform->setType('name', PARAM_TEXT);
+
+        $mform->addElement('hidden', 'cmid');
+        $mform->setType('cmid', PARAM_INT);
+
+        $this->add_action_buttons();
+    }
+}
